5ab2deb3b067c03d4e36f1f91a4ac5ffa07b40e1
NipoLau <NipoLau@icloud.com>
2019-05-03 08:02:33 -0700

    上传catkin_ws

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /home/nipo/AndroidStudioProjects/RobotApp/catkin_ws/src
+# Build directory: /home/nipo/AndroidStudioProjects/RobotApp/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(wpb_home/wpb_home_bringup)
+subdirs(wpb_home/wpb_home_remote)
+subdirs(rplidar_ros)
+subdirs(wpb_home/wpb_home_behaviors)
+subdirs(wpb_home/wpb_home_tutorials)
+subdirs(wpb_home/wpbh_local_planner)
