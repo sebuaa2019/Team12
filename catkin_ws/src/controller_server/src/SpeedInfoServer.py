a723af8e7441004981606c27945096bfe39b4870
ch1206643805 <34213366+ch1206643805@users.noreply.github.com>
2019-05-11 00:07:09 +0800

    Add files via upload

--- catkin_ws/src/controller_server/src/SpeedInfoServer.py
+++ catkin_ws/src/controller_server/src/SpeedInfoServer.py
@@ -8,17 +8,20 @@ class SpeedInfoServer:
     def __init__(self, host, port):
         self.soc = socket.socket()
         self.soc.bind((host, port))
+	self.con = None
 
     def sendtoclient(self, msg):
-        x = msg.linear.x
-        z = msg.angular.z
+        x = ('%.3f' % abs(msg.linear.x))
+        z = ('%.3f' % abs(msg.angular.z))
         if (x > 0 or z > 0):
             try:
-                self.soc.send('( ' + str(x) + ' , ' + str(z) + ' )')
+                self.con.send("("+str(x)+","+str(z)+")")
             except:
                 print('Connect Failed')
 
     def start(self, threadname):
         print('Info-Server Online!')
+	self.soc.listen(5)
+	self.con, addr = self.soc.accept()
         rospy.Subscriber('/cmd_vel', Twist, self.sendtoclient)
         rospy.spin()
