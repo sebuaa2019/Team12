1fb5f4afaccb8a7022db1c17ba410627537d44e1
NipoLau <NipoLau@icloud.com>
2019-05-18 23:14:11 -0700

    FIX

--- app/src/main/java/team/se/robotapp/ControActivity.java
+++ app/src/main/java/team/se/robotapp/ControActivity.java
@@ -83,7 +83,7 @@ public class ControActivity extends AppCompatActivity {
                 Switch = DISPLAYCAM;
             }
         });
-        navButton.setOnClickListener(new View.OnClickListener() {
+        mapButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Switch = DISPLAYMAP;
